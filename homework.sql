d615fa768fe3ce22194390a39e1e708b58996b5c
Dhartiben Patel <dhartipatel89@gmail.com>
2023-04-11 19:25:34 -0500

    Add files via upload

--- homework.sql
+++ homework.sql
@@ -23,14 +23,13 @@ WHERE amount BETWEEN 3.99 and 5.99;
 SELECT film_id, COUNT(film_id) AS film_count
 FROM inventory
 GROUP BY film_id
-HAVING COUNT(film_id) = (
-	SELECT MAX(film_count)
-	FROM (
-		SELECT film_id, COUNT(film_id) AS film_count
-		FROM inventory
-		GROUP BY film_id
-	) AS max_count
-);
+HAVING COUNT(film_id) = MAX(COUNT(film_id));
+
+SELECT film_id, COUNT(film_id) AS fc
+FROM inventory
+GROUP BY film_id
+ORDER BY fc DESC;
+
 
 -- 4. How many customers have the last name ‘William’?
 SELECT * 
@@ -55,14 +54,9 @@ WHERE last_name = 'Williams';
 SELECT staff_id, COUNT(rental_id) AS rental_count
 FROM rental
 GROUP BY staff_id
-HAVING COUNT(rental_id) = (
-	SELECT MAX(rental_count)
-	FROM (
-		SELECT staff_id, COUNT(rental_id) AS rental_count
-		FROM rental
-		GROUP BY staff_id
-	) AS max_count
-);
+ORDER BY COUNT(rental_id) DESC
+LIMIT 1;
+
 -- 6. How many different district names are there?
 
 SELECT COUNT(DISTINCT first_name)
@@ -73,17 +67,11 @@ FROM customer;
 
 -- 7. What film has the most actors in it? (use film_actor table and get film_id)
 
-SELECT film_id, COUNT(actor_id) 
+SELECT film_id, COUNT(actor_id) AS actor_count
 FROM film_actor
 GROUP BY film_id
-HAVING COUNT(actor_id) = (
-	SELECT MAX(actor)
-	FROM (
-		SELECT film_id, COUNT(actor_id) AS actor
-		FROM film_actor
-		GROUP BY film_id
-	) AS max_count
-);
+ORDER BY actor_count DESC
+LIMIT 1;
 
 
 -- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
@@ -95,31 +83,27 @@ GROUP BY store_id
 HAVING store_id = 1;
 
 -- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers with ids between 380 and 430? (use group by and having > 250)
-SELECT COUNT(amount_count)
-FROM (
-	SELECT COUNT(amount) as amount_count
-	FROM payment
-	WHERE customer_id BETWEEN 380 AND 430
-	GROUP BY amount
-	HAVING COUNT(rental_id) > 250
-) AS count_count;
+
+SELECT COUNT(amount)
+FROM payment
+WHERE customer_id BETWEEN 380 AND 430
+GROUP BY amount
+HAVING COUNT(rental_id) > 250;
+
+SELECT COUNT(*)
+FROM payment
+WHERE customer_id BETWEEN 380 AND 430
+GROUP BY amount
+HAVING COUNT(rental_id) > 250;
 
 -- 10. Within the film table, how many rating categories are there? And what rating has the most movies total?
 
-SELECT (SELECT COUNT(DISTINCT(rating)) FROM film), rating
-FROM (
-	SELECT rating
-	FROM film
-	GROUP BY rating
-	HAVING COUNT(rating) = (
-		SELECT MAX(rating_count)
-		FROM (
-			SELECT COUNT(rating) AS rating_count
-			FROM film
-			GROUP BY rating
-		) AS max_count
-	)
-) AS most_rated;
+SELECT COUNT(DISTINCT rating) AS rating_categories,
+rating AS most_common_rating, COUNT(*) AS total_movies
+FROM film
+GROUP BY rating
+ORDER BY total_movies DESC
+LIMIT 1;
 
 
 
